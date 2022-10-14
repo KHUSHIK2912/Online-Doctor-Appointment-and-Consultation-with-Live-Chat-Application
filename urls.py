62037bbb85e55c52e4f875afc3b525baa02b369f
KHUSHIK2912 <89707359+KHUSHIK2912@users.noreply.github.com>
2022-10-15 01:43:05 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -2,27 +2,17 @@
 doctor_appointment_system URL Configuration
 
 """
-
-from django.urls import path
-from appointment.views import *
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-app_name = 'appointment'
+from appointment.views import PatientListView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    path('', include('appointment.urls')),
+]
 
-    path('', HomePageView.as_view(), name='home'),
-    path('service', ServiceView.as_view(), name='service'),
-    path('doctor/appointment/create', AppointmentCreateView.as_view(), name='doctor-appointment-create'),
-    path('doctor/appointment/', AppointmentListView.as_view(), name='doctor-appointment'),
-    path('<pk>/delete/', AppointmentDeleteView.as_view(), name='delete-appointment'),
-    path('<pk>/patient/delete', PatientDeleteView.as_view(), name='delete-patient'),
-    path('patient-take-appointment', TakeAppointmentView.as_view(), name='take-appointment'),
-    path('search/', SearchView.as_view(), name='search'),
-    path('patient/', PatientListView.as_view(), name='patient-list'),
-    #path('patients/<int:appointment_id>', PatientPerAppointmentView.as_view(), name='patient-list'),
-
-
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
